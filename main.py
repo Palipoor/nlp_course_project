e04da511fe5535650b0daeac627c3134be8dff9f
Pegah Alipoormolabashi <palipoor976@gmail.com>
2022-12-02 11:27:06 -0500

    Change to 5 labels

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
             # forward pass
             outputs = model(**inputs)
             logits = outputs.get("logits")
-            loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([3.0, 1.0])).to('cuda')
+            loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 1.0, 2.0, 3.0, 1.0])).to('cuda')
             loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
             return (loss, outputs) if return_outputs else loss
 
