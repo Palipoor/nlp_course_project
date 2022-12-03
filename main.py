eec46a3d83209d98bdf60cbefc59f89a2239487b
Pegah Alipoormolabashi <palipoor976@gmail.com>
2022-12-03 11:47:25 -0500

    update everything

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import json
 
 import torch
-from scipy import stats
+from sklearn.metrics import accuracy_score
 from torch import nn
 from datasets import Dataset, DatasetDict
 from transformers import AutoModelForSequenceClassification, AutoModelForSeq2SeqLM, AutoTokenizer, TrainingArguments, \
@@ -16,6 +16,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-t2t', action='store_true')
+    parser.add_argument('--num_labels', default=2, type=int)
     parser.add_argument('--mode', type=str, help='train or test mode', required=True, choices=['train', 'test'])
     parser.add_argument('--expt_dir', type=str, help='root directory to save model & summaries')
     parser.add_argument('--expt_name', type=str, help='expt_dir/expt_name: organize experiments')
@@ -28,54 +29,49 @@ def main():
 
     args = parser.parse_args()
 
-    class CustomTrainer(Trainer):
-        def compute_loss(self, model, inputs, return_outputs=False):
-            labels = inputs.get("labels")
-            # forward pass
-            outputs = model(**inputs)
-            logits = outputs.get("logits")
-            loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([3.0, 3.0, 1.5, 1.0, 4.0])).to('cuda')
-            loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
-            return (loss, outputs) if return_outputs else loss
-
     n_epochs = args.epochs
     batch_size = args.batch_size
     lr = args.lr
-    accumulation_steps = args.acc_step
     data_dir = args.data_dir
     train_file = os.path.join(data_dir, 'train.json')
     val_file = os.path.join(data_dir, 'val.json')
     test_file = os.path.join(data_dir, 'test.json')
     output_dir = args.expt_dir
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
     if args.mode == 'train':
         with open(train_file, 'r') as f:
             train_data = json.loads(f.read())
-            train_instances = [create_nli_instance(x) for x in train_data]
+            if args.t2t:
+                train_instances = [t2t_preprocess_data(x, tokenizer) for x in train_data]
+            else:
+                train_instances = [preprocess_data(x, tokenizer) for x in train_data]
         with open(val_file, 'r') as f:
             val_data = json.loads(f.read())
-            val_instances = [create_nli_instance(x) for x in val_data]
+            if args.t2t:
+                val_instances = [t2t_preprocess_data(x, tokenizer) for x in val_data]
+            else:
+                val_instances = [preprocess_data(x, tokenizer) for x in val_data]
         with open(test_file, 'r') as f:
             test_data = json.loads(f.read())
-            test_instances = [create_nli_instance(x) for x in test_data]
+            if args.t2t:
+                test_instances = [t2t_preprocess_data(x, tokenizer) for x in test_data]
+            else:
+                test_instances = [preprocess_data(x, tokenizer) for x in test_data]
 
         if not args.t2t:
-            model = AutoModelForSequenceClassification.from_pretrained(args.model, num_labels=5)
+            model = AutoModelForSequenceClassification.from_pretrained(args.model, num_labels=args.num_labels)
         else:
             model = AutoModelForSeq2SeqLM.from_pretrained(args.model)
-        tokenizer = AutoTokenizer.from_pretrained(args.model)
         name = f"{args.expt_name}_bs_{batch_size}_lr_{lr}_epoch_{n_epochs}"
         run_name = os.path.join(output_dir, name)
 
         if not args.t2t:
-            train_data_points = [preprocess_data(x, tokenizer) for x in train_instances]
-            val_data_points = [preprocess_data(x, tokenizer) for x in val_instances]
-            test_data_points = [preprocess_data(x, tokenizer) for x in test_instances]
             dataset = DatasetDict({
-                'train': Dataset.from_list(train_data_points),
-                'valid': Dataset.from_list(val_data_points),
-                'test': Dataset.from_list(test_data_points),
+                'train': Dataset.from_list(train_instances),
+                'valid': Dataset.from_list(val_instances),
+                'test': Dataset.from_list(test_instances),
             })
-            print(sum([x['labels'] for x in train_data_points]))
+            print(sum([x['labels'] for x in train_instances]))
             train_args = TrainingArguments(
                 output_dir=run_name,
                 do_train=True,
@@ -91,7 +87,7 @@ def main():
                 load_best_model_at_end=True,
                 save_total_limit=2,
             )
-            trainer = CustomTrainer(
+            trainer = Trainer(
                 model,
                 train_args,
                 train_dataset=dataset["train"],
@@ -99,6 +95,7 @@ def main():
                 tokenizer=tokenizer,
             )
         else:
+
             train_data_points = [t2t_preprocess_data(x, tokenizer) for x in train_instances]
             val_data_points = [t2t_preprocess_data(x, tokenizer) for x in val_instances]
             test_data_points = [t2t_preprocess_data(x, tokenizer) for x in test_instances]
@@ -140,17 +137,17 @@ def main():
                 input_ids = torch.Tensor(d['input_ids']).to(torch.int).reshape(1, -1).to('cuda')
                 attn_mask = torch.Tensor(d['attention_mask']).to(torch.int).reshape(1, -1).to('cuda')
                 result = model(input_ids=input_ids, attention_mask=attn_mask)
-                result =  torch.argmax(result.logits).item()
-                narrative, question, answer, human_score = get_important_parts(d['meta'])
-                preds.append((f'"{narrative}"', f'"{question}"', f'"{answer}"', human_score, str(result)))
+                result = torch.argmax(result.logits).item()
+                narrative, question, answer, human_score, label = get_important_parts(d)
+                preds.append((f'"{narrative}"', f'"{question}"', f'"{answer}"', human_score, label, result))
                 pred_numbers.append(result)
-                references.append(int(human_score))
+                references.append(label)
             with open(results_dir, 'w') as out:
-                out.write('narrative,question,answer,human_score_sum,prediction' + '\n')
+                out.write('narrative,question,answer,human_score_avg,prediction' + '\n')
                 for p in preds:
                     out.write(','.join(p) + '\n')
-            corr = stats.spearmanr(references, pred_numbers)
-            print(corr)
+            accuracy = accuracy_score(references, pred_numbers)
+            print(accuracy)
         else:
             for d in dataset['test']:
                 input_ids = torch.Tensor(d['input_ids']).to(torch.int).reshape(1, -1).to('cuda')
--- utils.py
+++ utils.py
@@ -1,50 +1,69 @@
-def create_nli_instance(qa_instance):
-    context = qa_instance['narrative']
-    effect = qa_instance['original_sentence_for_question']
-    cause = qa_instance['answer']
-
-    premise = context
-    if 'because' in cause:
-        hypothesis = cause
-    else:
-        hypothesis = effect[:-1] + ' because ' + cause
-    return {
-        'premise': premise,
-        'hypothesis': hypothesis,
-        'meta': qa_instance
-    }
+import argparse
+import sys
+import pickle
+import pandas as pd
+import json
+import re
 
 
-def preprocess_label(instance):
-    scores = instance['meta']['val_ann']
-    if sum(scores) >= 3:
-        return sum(scores) - 2
-    return 0
+# def create_nli_instance(qa_instance):
+#     context = qa_instance['narrative']
+#     effect = qa_instance['original_sentence_for_question']
+#     cause = qa_instance['answer']
+#
+#     premise = context
+#     if 'because' in cause:
+#         hypothesis = cause
+#     else:
+#         hypothesis = effect[:-1] + ' because ' + cause
+#     return {
+#         'premise': premise,
+#         'hypothesis': hypothesis,
+#         'meta': qa_instance
+#     }
 
 
+# def preprocess_label(instance):
+#     scores = instance['meta']['val_ann']
+#     if sum(scores) >= 3:
+#         return sum(scores) - 2
+#     return 0
+
+#
 def t2t_preprocess_label(instance):
-    scores = instance['meta']['val_ann']
-    if sum(scores) >= 4:
-        return 'entailment'
-    return 'not_entailment'
+    score = instance['label']
+    if score == 1:
+        return 'valid'
+    else:
+        return 'invalid'
 
 
 def t2t_preprocess_data(instance, tokenizer):
-    input_text = 'tmw hypothesis: ' + instance['hypothesis'] + ' premise: ' + instance['premise']
+    input_text = 'tmw question: ' + instance['question'] + ' context: ' + instance['narrative'] + ' answer: ' + \
+                 instance['answer']
     encoded = tokenizer(input_text, padding='max_length', max_length=256)
     tokenized_input = {'input_ids': encoded.input_ids}
     label = t2t_preprocess_label(instance)
-    output = tokenizer(label, padding='max_length', max_length=10).input_ids
-    tokenized_input.update({'labels': output, 'meta': instance['meta']})
+    output = tokenizer(label, padding='max_length', max_length=5).input_ids
+    tokenized_input.update({'labels': output, 'meta': {'answer': instance['answer'],
+                                                       'question': instance['question'],
+                                                       'narrative': instance['narrative'],
+                                                       'human_score': instance['avg_score'],
+                                                       'label': instance['label']}})
     return tokenized_input
 
 
 def preprocess_data(instance, tokenizer):
-    tokenized_input = tokenizer(instance['premise'], instance['hypothesis'], add_special_tokens=True, padding=True)
-    label = preprocess_label(instance)
-    tokenized_input.update({'labels': label, 'meta': instance['meta']})
+    tokenized_input = tokenizer(instance['narrative'], instance['question'], instance['answer'],
+                                add_special_tokens=True, padding=True)
+    label = instance['label']
+    tokenized_input.update({'labels': label, 'meta': {'answer': instance['answer'],
+                                                      'question': instance['question'],
+                                                      'narrative': instance['narrative'],
+                                                      'human_score': instance['avg_score'],
+                                                      'label': instance['label']}})
     return tokenized_input
 
 
 def get_important_parts(data):
-    return data['narrative'], data['question'], data['answer'], str(sum(data['val_ann']))
+    return data['narrative'], data['question'], data['answer'], data['human_score'], data['label']
